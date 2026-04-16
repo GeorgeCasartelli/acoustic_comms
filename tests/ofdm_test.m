695e537d9fa4517a2fc7d1ce34c1f89bbfcb748f
George <georgecasartelli@gmail.com>
2026-04-16 11:37:43 +0100

    Added pilots into the signal and cleaned up active carrier logic

--- tests/ofdm_test.m
+++ tests/ofdm_test.m
@@ -2,30 +2,29 @@ clear all; clc;
 
 M = 4;
 k = log2(M);
-nfft = 1024;
+nfft = 512;
 cplen = 32;
 fs = 48000;
 fc = 6000;
 
 % define active carriers around the midpoint of nfft val
-numDataCarriers = 20;
-spacing = 2; % attempt at interleaving
+numActiveCarriers = 42;
+pilotSpacing = 10;
 
-span = (numDataCarriers - 1) * spacing;
 % indices for first and second half of points
-firstHalf = (nfft/2) - (span/2);
-secondHalf = (nfft/2) + (span/2);
-% activeCarriers = (firstHalf : spacing: secondHalf).';
-activeCarriers = (firstHalf : spacing : secondHalf).';
+activeCarriers = ((nfft/2) - (numActiveCarriers/2) : (nfft/2) + (numActiveCarriers/2)).';
+
 activeCarriers(activeCarriers == nfft/2 + 1) = []; % remove dc 0 
+pilotIdx = activeCarriers(1:pilotSpacing:end);
+dataIdx = setdiff(activeCarriers, pilotIdx);
+
 nullIdx = setdiff(1:nfft, activeCarriers).'; % define null carrier idx
 
-numDataCarriers = length(activeCarriers);
 
-% msg = 'giggle piggle. ';
-file = fopen("textfile.txt","r")';
-msg = fscanf(file, "%c");
-fclose(file);
+msg = 'its really late and i need to go eat but the message gets corrupted because the receiver doesnt know how long the message is in the slightest. really not ideal';
+% file = fopen("textfile.txt","r")';
+% msg = fscanf(file, "%c");
+% fclose(file);
 
 
 constSym = pskmod((0:M-1), M, pi/4); 
@@ -42,22 +41,27 @@ binchars = dec2bin(msg, 8);
 bits = reshape(binchars.' - '0', [], 1); 
 totalBits = numel(bits); % will be included in packet header eventually
 
-bitsPerFrame = numDataCarriers * k;
+bitsPerFrame = length(dataIdx) * k;
 nFrames = ceil(numel(bits) / bitsPerFrame);
 paddedBits = [bits; zeros(nFrames*bitsPerFrame - totalBits, 1)]; % pad to make square. 
 bitgroups = reshape(paddedBits, k, [])'; % reshape by width k
 inputSymbols = bi2de(bitgroups, 'left-msb'); % conv to int
 
 % preamble = [0;1;2;3;0;1;2;3;0;1;2;3;0;1;2;3;3;2;1;0;3;2;1;0;3;2;1;0;3;2;1;0];
-preamble = mod(0:numDataCarriers-1, 4).';
+preamble = mod(0:numActiveCarriers-1, 4).';
 preambleSignal = pskmod(preamble, M, pi/4);
 
 
 % organise data into carriers x symbols matrix
-qpskSig = reshape(pskmod(inputSymbols, M, pi/4), numDataCarriers, nFrames);  
-qpskSigFull = [preambleSignal, qpskSig];
+qpskSig = reshape(pskmod(inputSymbols, M, pi/4), length(dataIdx), []);  
+preambleData = preambleSignal(1:length(dataIdx));
 
-tx_bb = ofdmmod(qpskSigFull, nfft, cplen, nullIdx);
+qpskSigFull = [preambleData, qpskSig];
+
+
+pilots = repmat(pskmod(0,M,pi/4),length(pilotIdx),nFrames+1);
+
+tx_bb = ofdmmod(qpskSigFull, nfft, cplen, nullIdx, pilotIdx, pilots);
 
 symbolLen = nfft + cplen;
 roll = 16; % try 8–32 samples
@@ -100,12 +104,11 @@ disp("Tx done");
 %% ---- RX ----
 [rx, fs] = audioread("ofdm_signal.wav");
 
-
 t_rx = (0:length(rx)-1)' /fs; % new time vector
 rx_mixed = rx .* exp(-1j*2*pi*fc*t_rx); % mix down to baseband
 rx_baseband = rx_mixed;
 
-preamble_bb = ofdmmod(preambleSignal, nfft, cplen, nullIdx); % make a preamble reference
+preamble_bb = ofdmmod(preambleData, nfft, cplen, nullIdx, pilotIdx, pilots(:,1)); % make a preamble reference
 
 % rx_baseband = rx_mixed;
 
@@ -121,7 +124,7 @@ numSymbolsReceived = floor(length(rxAligned) / symbolLen);
 rxTrimmed = rxAligned(1 : numSymbolsReceived * symbolLen);
 % 
 % x1 = ofdmdemod(rxTrimmed, nfft, cplen, 0, nullIdx);
-x1 = ofdmdemod(rxTrimmed, nfft, cplen, 0, nullIdx);
+[x1, rxPilots] = ofdmdemod(rxTrimmed, nfft, cplen, 0, nullIdx, pilotIdx);
  
 rxPreamble = x1(:, 1); % get first symbol (preamble) 
 rxDataSyms = x1(:, 2:end); % get payload
@@ -130,10 +133,19 @@ disp(preamble);
 disp(pskdemod(rxPreamble, M,pi/4));
 
 % un rotate the signal using known preamble and received
-H = rxPreamble ./ preambleSignal; 
+H = rxPreamble ./ preambleData; 
 x1_equalised = rxDataSyms ./ H;
 
 rxData = pskdemod(x1_equalised, M, pi/4);
+
+rxPilotSym = pskdemod(rxPilots,M,pi/4);
+originalPilotSym = pskdemod(pilots, M, pi/4);
+
+if isequal(rxPilotSym,originalPilotSym)
+    disp("perfect pilots!");
+else
+    disp("uh oh bad pilots");
+end
 
 %% ---- DATA CHECK AND RECOVERY ----
 isequal(rxData(:),inputSymbols)
@@ -155,35 +167,35 @@ outputText = bin2dec(charArray).';
 fprintf('Recovered: %s\n', char(outputText));
 
 
-figure('Name', 'Power Spectral Density');
-[pxx, f] = pwelch(txPassband, hanning(1024), 512, 1024, fs); 
-
-plot(f/1000, 10*log10(pxx), 'LineWidth', 1.5, 'Color', [0 0.447 0.741]);
-grid on;
-xlabel('Frequency (kHz)');
-ylabel('Power/Frequency (dB/Hz)');
-title('PSD of the Transmitted OFDM Signal');
-
-% Zoom in on the area of interest (fc +/- a few kHz)
-xlim([(fc-5000)/1000, (fc+5000)/1000]);
-
-%% ---- VISUALIZING THE 2*fc COMPONENT ----
-% Calculate the spectrum of the mixed (but unfiltered) signal
-[pxx_mixed, f_mixed] = pwelch(rx_mixed, hanning(1024), 512, 1024, fs, 'centered');
-
-figure('Name', 'Justification for Lowpass Filter');
-plot(f_mixed/1000, 10*log10(pxx_mixed), 'LineWidth', 1.5);
-grid on;
-hold on;
-
-% Label the parts
-xlabel('Frequency (kHz)');
-ylabel('Magnitude (dB)');
-title('Spectrum of rx\_mixed (Before Filtering)');
-
-% Draw a box or line around the parts to justify the filter
-xline(0, '--r', 'Baseband (Data)', 'LabelVerticalAlignment', 'bottom');
-xline(-2*fc/1000, '--k', 'Image at -2fc'); % If using complex mixer
-xline(2*fc/1000, '--k', 'Image at 2fc');
-
-legend('Signal Spectrum', 'Target Data (0 Hz)', 'Unwanted Image');
+% figure('Name', 'Power Spectral Density');
+% [pxx, f] = pwelch(txPassband, hanning(1024), 512, 1024, fs); 
+% 
+% plot(f/1000, 10*log10(pxx), 'LineWidth', 1.5, 'Color', [0 0.447 0.741]);
+% grid on;
+% xlabel('Frequency (kHz)');
+% ylabel('Power/Frequency (dB/Hz)');
+% title('PSD of the Transmitted OFDM Signal');
+% 
+% % Zoom in on the area of interest (fc +/- a few kHz)
+% xlim([(fc-5000)/1000, (fc+5000)/1000]);
+% 
+% %% ---- VISUALIZING THE 2*fc COMPONENT ----
+% % Calculate the spectrum of the mixed (but unfiltered) signal
+% [pxx_mixed, f_mixed] = pwelch(rx_mixed, hanning(1024), 512, 1024, fs, 'centered');
+% 
+% figure('Name', 'Justification for Lowpass Filter');
+% plot(f_mixed/1000, 10*log10(pxx_mixed), 'LineWidth', 1.5);
+% grid on;
+% hold on;
+% 
+% % Label the parts
+% xlabel('Frequency (kHz)');
+% ylabel('Magnitude (dB)');
+% title('Spectrum of rx\_mixed (Before Filtering)');
+% 
+% % Draw a box or line around the parts to justify the filter
+% xline(0, '--r', 'Baseband (Data)', 'LabelVerticalAlignment', 'bottom');
+% xline(-2*fc/1000, '--k', 'Image at -2fc'); % If using complex mixer
+% xline(2*fc/1000, '--k', 'Image at 2fc');
+% 
+% legend('Signal Spectrum', 'Target Data (0 Hz)', 'Unwanted Image');
